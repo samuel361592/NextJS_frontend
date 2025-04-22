4075e589ec6ba0545f4cbae10f8d2018c107755b
samuel361592 <samuel361592@gmail.com>
2025-04-22 09:16:36 +0800

    新增 post/user types 並清理不必要檔案

---
+++ types/post.ts
@@ -0,0 +1,8 @@
+import { User } from './user'
+
+export interface Post {
+  id: number
+  title: string
+  content: string
+  user: User
+}
---
+++ types/user.ts
@@ -0,0 +1,11 @@
+import { Post } from './post'
+
+export interface User {
+  id: number
+  name: string
+  email: string
+  password: string
+  age: number
+  posts: Post[]
+  roles: string[] // 前端簡化處理，用字串陣列表示角色（如果有需要也可以改成 Role type）
+}
